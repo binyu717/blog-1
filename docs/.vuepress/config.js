934430fe708599c847175af5b0907f4a55be0ae3
WanPeng <986063761@qq.com>
2018-06-05 23:39:27 +0800

    项目初始化更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,23 @@
 module.exports = {
-    title: '我的博客',
-    description: '我的博客',
-    base: '/blog/'
+  title: '君の名は',
+  description: '自从进入前端圈子以来，记录的一些自己所经历过的技术栈。',
+  base: '/blog/',
+  head: [
+    ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico` }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'vuepress', link: 'http://caibaojian.com/vuepress/' },
+    ],
+    sidebar: [
+      {
+        title: '基础',
+        collapsable: true,
+        children: [
+          ['/base/html', '所见html']
+        ]
+      }
+    ]
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,10 @@
-# 我的博客
+---
+home: true
+heroImage: /avatar.jpg
+actionText: 开始 →
+actionLink: /base/html
+features:
+- title: 基础
+  details: web技术的内功、决定着web开发者的深度、web灵魂之所在，好好把握我们的基础理论，不断的提升自己。
+footer: MIT Licensed | Copyright © 2018 君の名は
+---
